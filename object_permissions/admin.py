3e4130abc243ca6a375eac79308b4bd170235631
Christian Schilling <christian@initcrash.net>
2009-09-30 18:21:50 +0200

    use has_*_permission hooks in django admin class

--- object_permissions/admin.py
+++ object_permissions/admin.py
@@ -35,6 +35,20 @@ class ObjectPermissionsAdmin(admin.ModelAdmin):
     def is_deleteable(self,model,object_id,request):
         return UserObjectPermission.objects.is_deleteable(self.model,object_id,request.user)
 
+    def has_change_permission(self,request,object=None):
+        any = super(ObjectPermissionsAdmin,self).has_change_permission(request,object)
+        if not object:
+            return any
+        else:
+            return any and self.is_changeable(self.model,object.id,request)
+
+    def has_delete_permission(self,request,object=None):
+        any = super(ObjectPermissionsAdmin,self).has_delete_permission(request,object)
+        if not object:
+            return any
+        else:
+            return any and self.is_deleteable(self.model,object.id,request)
+
     def add_view(self,request,*args,**kwargs):
         self.inline_instances = [i for i in self.all_inline_instances
                                     if check_inline_perm(request.user,i)]
@@ -51,6 +65,7 @@ class ObjectPermissionsAdmin(admin.ModelAdmin):
         if self.is_deleteable(self.model,object_id,request):
             return super(ObjectPermissionsAdmin,self).delete_view(request,object_id,extra_context)
         raise PermissionDenied
+
 
     def save_model(self, request, obj, form, change):
         super(ObjectPermissionsAdmin,self).save_model(request,obj,form,change)
